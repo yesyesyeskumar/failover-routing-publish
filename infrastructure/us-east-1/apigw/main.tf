1b3958a00bfdb20ded612f4b61ff080b03eecce0
yesyesyeskumar <154976245+yesyesyeskumar@users.noreply.github.com>
2024-02-06 14:42:55 +0530

    Update main.tf

--- infrastructure/us-east-1/apigw/main.tf
+++ infrastructure/us-east-1/apigw/main.tf
@@ -125,7 +125,7 @@ resource "aws_lambda_permission" "user_api_lambda_permission" {
 
 resource "aws_api_gateway_domain_name" "app_domain" {
   regional_certificate_arn = var.cert_arn
-  domain_name     = "arn:aws:route53:::hostedzone/Z05777072YIYTEQL0T1RR"
+  domain_name     = "poc.ibmb.equitasbank.com"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
